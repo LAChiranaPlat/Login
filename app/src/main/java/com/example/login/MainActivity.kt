f8dd0d9404c540e93da5420568e8196542c3c8ec
huarseral <huarseral@hotmail.com>
2022-01-11 21:37:16 -0500

    Validación Login Fase 01

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,13 +1,47 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.util.Log
+import com.example.login.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var contentView:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        contentView= ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(contentView.root)
+
+        val btnAcces=contentView.btnIntro
+
+        val txtUser=contentView.txtusernick.editText
+        val txtPassUser=contentView.txtpassuser.editText
+
+        btnAcces.setOnClickListener { v->
+            val user=txtUser?.text.toString()
+            val pass=txtPassUser?.text.toString()
+
+            if(user=="huarseral" && pass=="developer21"){
+
+                val intent= Intent(this,system::class.java).apply{
+                    putExtra(EXTRA_MESSAGE,user)
+                }
+
+                startActivity(intent)
+
+            }else{
+
+            }
+
+
+        }
+
     }
 
 }
--- app/src/main/java/com/example/login/system.kt
+++ app/src/main/java/com/example/login/system.kt
@@ -2,12 +2,23 @@ package com.example.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import com.example.login.databinding.ActivitySystemBinding
 
 class system : templateMain() {
 
+    lateinit var contentView:ActivitySystemBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_system)
+
+        val user=intent.getStringExtra(EXTRA_MESSAGE)
+
+        contentView= ActivitySystemBinding.inflate(layoutInflater)
+        contentView.lblUser.text=user
+
+        setContentView(contentView.root)
+
     }
 
 }
