4f67e027d5f3e0f7777191b8a47f8daf4a7002b4
huarseral <huarseral@hotmail.com>
2022-01-14 21:44:39 -0500

    JSONRequest

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.util.Log
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.example.login.databinding.ActivityMainBinding
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var contentView:ActivityMainBinding
+    lateinit var colaPeticion:RequestQueue
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +32,35 @@ class MainActivity : AppCompatActivity() {
         val txtUser=contentView.txtusernick.editText
         val txtPassUser=contentView.txtpassuser.editText
 
+        colaPeticion=Volley.newRequestQueue(this)
+
+        var url="http://192.168.1.50:90/myApp/save.php"
+
         btnAcces.setOnClickListener { v->
+
+            val datosWeb=HashMap<String,Any?>()
+            datosWeb["name"]=txtUser?.text.toString()
+            datosWeb["dni"]="999999"
+            val _jsonReq=JSONObject(datosWeb)
+
+            val petObj=JsonObjectRequest(
+                Request.Method.POST,
+                url,
+                _jsonReq,
+                Response.Listener { response ->
+                    Log.i("result","Size: "+response.length())
+                    Log.i("result",response.get("proceso").toString())
+                    Log.i("result",response.get("message").toString())
+                },
+                Response.ErrorListener { response ->
+                    Log.i("result","Error: "+response.message.toString())
+                }
+            )
+
+            colaPeticion.add(petObj)
+
+
+            /*
             val user=txtUser?.text.toString()
             val pass=txtPassUser?.text.toString()
 
@@ -38,6 +75,8 @@ class MainActivity : AppCompatActivity() {
             }else{
 
             }
+            */
+
 
 
         }
